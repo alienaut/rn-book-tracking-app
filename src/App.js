675a463cb6143725a9711370dcd24cb7cd657135
Yusuf Yalim <yusufyalim@gmail.com>
2018-12-10 02:14:42 +0300

    List books from API on RootPage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import SearchPage from './SearchPage'
 import RootPage from './RootPage'
+import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = { }
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => { this.setState(() => ({ books })) })
+  }
 
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
-        <Route exact path="/" component={RootPage} />
+        <Route exact path="/" render={() => (
+          <RootPage books={books}/>
+        )} />
         <Route path="/search" component={SearchPage} />
       </div>
     )
